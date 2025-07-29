ec0138c81b20071f099e904bb34d01dc1858fe37
[dyad] <valik.shevhyk@gmail.com>
2025-07-29 16:17:57 +0300

    [dyad] Оновлення GIF-зображення в Кроці 4. - wrote 1 file(s) + extra files edited outside of Dyad

--- components/steps/Step4Content.tsx
+++ components/steps/Step4Content.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 const Step4Content: React.FC = () => {
   return (
     <div>
+      <div className="flex justify-center mb-6">
+        <img src="/ezgif-12484b51c5c8be.gif" alt="Uploading file" className="w-16 h-auto rounded-lg shadow-lg" />
+      </div>
       <p className="text-lg leading-relaxed text-gray-400 mb-5 border-l-4 border-purple-600 pl-4">
         Ти майже біля мети! Залишився останній крок. Тепер тобі потрібно завантажити файл <strong>sound-detective-answers.txt</strong>, який ти зберіг, на платформу Logika.
       </p>
